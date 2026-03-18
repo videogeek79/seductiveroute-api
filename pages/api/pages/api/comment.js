4218ee545043db5f4585aae60f59b0224a66c600
videogeek79 <64650944+videogeek79@users.noreply.github.com>
2026-03-18 18:21:12 -0500

    Delete pages/api/pages/api directory

--- pages/api/pages/api/comment.js
+++
@@ -1,6 +0,0 @@
-export default function handler(req, res) {
-  const { name } = req.query;
-  res.status(200).json({
-    comment: `Stay focused, ${name}. Naughty Navigator's got you on this route.`
-  });
-}
